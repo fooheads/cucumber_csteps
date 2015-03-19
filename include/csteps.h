178ee706b1d64deac9765f87202b84165012b5b1
Jon-Erling Dahl <joda@soloduo.se>
2015-03-19 16:19:32 +0100

    Include csteps.h for easy usage

---
+++ include/csteps.h
@@ -0,0 +1,8 @@
+#define CONCAT_(x, y) x ## y
+#define CONCAT(x, y) CONCAT_(x, y)
+
+#define STEP(x) extern "C" void CONCAT(STEP_PREFIX, __LINE__)
+#define GIVEN STEP
+#define WHEN STEP
+#define THEN STEP
+
