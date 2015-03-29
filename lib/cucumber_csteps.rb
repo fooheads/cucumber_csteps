d80c2ff7a7e1e1a4bbfca99b03649534c6718728
Jon-Erling Dahl <joda@soloduo.se>
2015-03-29 19:52:00 +0200

    Extract c snippets hack to separate file

--- lib/cucumber_csteps.rb
+++ lib/cucumber_csteps.rb
@@ -1,6 +1,8 @@
 require 'cucumber_csteps/version'
 require 'cucumber_csteps/parser'
 require 'cucumber_csteps/define_steps'
+require 'cucumber_csteps/c_snippets'
+
 
 module CucumberCsteps
   def self.include_path
--- lib/cucumber_csteps/define_steps.rb
+++ lib/cucumber_csteps/define_steps.rb
@@ -1,45 +1,6 @@
 require 'ffi'
 require_relative 'step_wrapper'
 
-require 'cucumber/rb_support/snippet.rb'
-
-# Hack to output C step definitions
-module Cucumber
-  module RbSupport
-    module Snippet
-      class Regexp < BaseSnippet
-        def initialize(code_keyword, pattern, multiline_argument)
-          super(code_keyword.upcase, pattern, multiline_argument)
-        end
-
-        def do_block
-          do_block = ""
-          do_block << "(#{arguments}) {\n"
-          #multiline_argument.append_comment_to(do_block)
-          do_block << "  //pending(); // Write code here that turns the phrase above into concrete actions\n"
-          do_block << "}"
-          do_block
-        end
-
-        def arguments
-          block_args = (0...number_of_arguments).map { |n| "some_t arg#{n+1}" }
-          block_args.empty? ? "" : "#{block_args.join(", ")}"
-        end
-
-        def typed_pattern
-          @code_keyword = @code_keyword.upcase
-          "(\"^#{pattern}$\")"
-        end
-
-        def self.description
-          "Snippets with parentheses"
-        end
-      end
-    end
-  end
-end
-
-
 module CucumberCsteps
 
   TYPE_MAP = {
