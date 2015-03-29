805c53913d6a3d66eeda1de78d4e02562c1653cd
Jon-Erling Dahl <joda@soloduo.se>
2015-03-29 21:41:05 +0200

    Add csteps-include-path binary to help locate csteps.h

--- lib/cucumber_csteps.rb
+++ lib/cucumber_csteps.rb
@@ -3,9 +3,3 @@ require 'cucumber_csteps/parser'
 require 'cucumber_csteps/define_steps'
 require 'cucumber_csteps/c_snippets'
 
-
-module CucumberCsteps
-  def self.include_path
-    File.expand_path(File.join(__FILE__, '..', 'include'))
-  end
-end
